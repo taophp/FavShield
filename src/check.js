c1e0278ce313e62e80258b6a1fad57bf9358dc31
Stéphane Mourey <s+github@stephanemourey.fr>
2023-06-27 15:39:09 +0200

    Improve hash algo

--- src/check.js
+++ src/check.js
@@ -14,14 +14,10 @@
     xhr.send();
   }
 
-  function hash(data, algorithm) {
-    var encoder = new TextEncoder();
-    var dataBuffer = encoder.encode(data);
-    return crypto.subtle.digest(algorithm, dataBuffer).then(function (hashBuffer) {
-      var hashArray = Array.from(new Uint8Array(hashBuffer));
-      var hashString = hashArray.map(function (byte) {
-        return byte.toString(16).padStart(2, '0');
-      }).join('');
+  function hash(data) {
+    var dataBuffer = new TextEncoder().encode(data);
+    return crypto.subtle.digest("SHA-256", dataBuffer).then(function (hashBuffer) {
+      var hashString = new TextDecoder().decode(new Uint8Array(hashBuffer));
       return hashString;
     });
   }
@@ -29,15 +25,9 @@
   function handleFiles(index) {
     if (index === files.length) {
       var pageContent = fileContents.concat([document.documentElement.outerHTML]);
-      hash(pageContent.join(""), "SHA-256").then(function (hashValue) {
-        if (hashValue === expectedHash) {
-          var securedFields = document.querySelectorAll("input[data-js-secured=\"true\"], textarea[data-js-secured=\"true\"]");
-          for (var i = 0; i < securedFields.length; i++) {
-            securedFields[i].disabled = false;
-          }
-        } else {
-          alert("Unable to unlock the form : integrity of the cannot be verified !")
-        }
+      hash(pageContent.join("")).then(function (hashValue) {
+        navigator.clipboard.writeText(hashValue);
+        alert(hashValue);
       });
       return;
     }
