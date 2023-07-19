39174b52c8043a8a2fb00a2106f22a7140004e46
Stéphane Mourey <s+github@stephanemourey.fr>
2023-07-19 18:45:39 +0200

    Fix JavaScript for Firefox, replace XHR buy fetch

--- src/check.js
+++ src/check.js
@@ -1,63 +1,55 @@
-(function () {
-  var expectedHash = "<PUT YOUR HASH HERE>";
-  var files = [];
-  var fileContents = [];
+  (function () {
+    var files = [];
+    var fileContents = [];
+    var expectedHash = "<PUT YOU HASH HERE>";
 
-  function loadFile(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        callback(xhr.responseText);
-      }
-    };
-    xhr.send();
-  }
-
-  function hash(data) {
-    var dataBuffer = new TextEncoder().encode(data);
-    return crypto.subtle.digest("SHA-256", dataBuffer).then(function (hashBuffer) {
-      var hashArray = Array.from(new Uint8Array(hashBuffer));
-      return hashArray.map(function (byte) {
-        return byte.toString(16).padStart(2, '0');
-      }).join('');
-    });
-  }
-
-  function handleFiles(index) {
-    if (index === files.length) {
-      var pageContent = fileContents.concat([document.documentElement.outerHTML]);
-      hash(pageContent.join("")).then(function (hashValue) {
-        if (hashValue === expectedHash) {
-          var securedFields = document.querySelectorAll("input[data-js-secured=\"true\"], textarea[data-js-secured=\"true\"]");
-          for (var i = 0; i < securedFields.length; i++) {
-            securedFields[i].disabled = false;
-          }
-        } else {
-          alert("Unable to unlock the form : integrity of the cannot be verified !");
-        }
-    });
-      return;
+    function hash(data) {
+      var dataBuffer = new TextEncoder().encode(data);
+      return crypto.subtle.digest("SHA-256", dataBuffer).then(function (hashBuffer) {
+        var hashArray = Array.from(new Uint8Array(hashBuffer));
+        return hashArray.map(function (byte) {
+          return byte.toString(16).padStart(2, "0");
+        }).join("");
+      });
     }
-    loadFile(files[index], function (content) {
-      fileContents[index] = content;
-      handleFiles(index + 1);
+
+    function handleFiles(index) {
+      if (index === files.length) {
+        var pageContent = fileContents.concat([document.documentElement.outerHTML]);
+        var join = pageContent.join("");
+        hash(pageContent.join("")).then(function (hashValue) {
+          if (hashValue === expectedHash) {
+            var securedFields = document.querySelectorAll("input[data-js-secured=\"true\"], textarea[data-js-secured=\"true\"]");
+            for (var i = 0; i < securedFields.length; i++) {
+              securedFields[i].disabled = false;
+            }
+            alert("Form unlocked !");
+          } else {
+            alert("Unable to unlock the form : integrity of the cannot be verified !\nHash found: " + hashValue + "\nHash expected: " + expectedHash);
+          }
+        });
+        return;
+      }
+      fetch(files[index])
+        .then(function (content) {
+          fileContents[index] = content;
+          handleFiles(index + 1);
+        });
+    }
+
+    var scripts = Array.from(document.querySelectorAll("script[src]")).map(function (script) {
+      return script.src;
     });
-  }
 
-  var scripts = Array.from(document.querySelectorAll("script[src]")).map(function (script) {
-    return script.src;
-  });
+    var stylesheets = Array.from(document.querySelectorAll("link[rel=\"stylesheet\"][href]")).map(function (link) {
+      return link.href;
+    });
 
-  var stylesheets = Array.from(document.querySelectorAll("link[rel=\"stylesheet\"][href]")).map(function (link) {
-    return link.href;
-  });
+    var images = Array.from(document.querySelectorAll("img[src]")).map(function (image) {
+      return image.src;
+    });
 
-  var images = Array.from(document.querySelectorAll("img[src]")).map(function (image) {
-    return image.src;
-  });
+    files = scripts.concat(stylesheets, images);
 
-  files = scripts.concat(stylesheets, images);
-
-  handleFiles(0);
-})();
+    handleFiles(0);
+  })();
--- src/hash.js
+++ src/hash.js
@@ -2,40 +2,31 @@
   var files = [];
   var fileContents = [];
 
-  function loadFile(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        callback(xhr.responseText);
-      }
-    };
-    xhr.send();
-  }
-
   function hash(data) {
     var dataBuffer = new TextEncoder().encode(data);
     return crypto.subtle.digest("SHA-256", dataBuffer).then(function (hashBuffer) {
       var hashArray = Array.from(new Uint8Array(hashBuffer));
       return hashArray.map(function (byte) {
-        return byte.toString(16).padStart(2, '0');
-      }).join('');
+        return byte.toString(16).padStart(2, "0");
+      }).join("");
     });
   }
 
   function handleFiles(index) {
     if (index === files.length) {
       var pageContent = fileContents.concat([document.documentElement.outerHTML]);
-      hash(pageContent.join("")).then(function (hashValue) {
+      var join = pageContent.join("");
+      hash(join).then(function (hashValue) {
         navigator.clipboard.writeText(hashValue);
         alert(hashValue);
       });
       return;
     }
-    loadFile(files[index], function (content) {
-      fileContents[index] = content;
-      handleFiles(index + 1);
-    });
+    fetch(files[index])
+      .then(function (content) {
+        fileContents[index] = content;
+        handleFiles(index + 1);
+      });
   }
 
   var scripts = Array.from(document.querySelectorAll("script[src]")).map(function (script) {
